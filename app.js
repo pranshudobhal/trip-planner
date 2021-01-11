d19bc2be92e83b6025748de6e164150b73af55a5
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-01-12 03:52:53 +0530

    updated delete from api

--- app.js
+++ app.js
@@ -16,21 +16,27 @@ function displayData(data) {
     //construct card element
     const card = document.createElement('div');
     card.classList = 'row';
-    card.setAttribute('id', 'card' + id);
+    card.setAttribute('id', 'card' + results.id);
+
+    var d = new Date(results.start);
+    var date = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear() + ', ' + d.toLocaleTimeString([], { timeStyle: 'short' });
 
     //construct card content
     const content = `
-    <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">mode_edit</i></a>
-    <a class="btn-floating btn-large waves-effect waves-light red" onClick=deleteID(${results.id})>
+    <a class="btn-floating btn-large waves-effect waves-light red" onClick=modifyDestination(${results.id}) ><i class="material-icons">mode_edit</i></a>
+    <a class="btn-floating btn-large waves-effect waves-light red" onClick=deleteData(${results.id})>
     <i class="large material-icons">delete</i>
+    <a class="btn-floating btn-large waves-effect waves-light red" id="save${results.id}">
+    <i class="large material-icons">done</i>
     </a>
         <div class="col s12 m6">
             <div class="card" style="background-color: ${results.color}">
                 <div class="card-content white-text">
-                  <span class="card-title">${results.destination}</span>
-                  <p><strong>Start Date: </strong><span class="date">${results.start}</span></p>
-                  <p><strong>Duration: </strong><span class="duration">${results.duration}</span></p>
-                  <p><strong>Comments: </strong><span class="comments">${results.comment}</span></p>
+                <p>
+                  <span id = "destination${results.id}" contenteditable="true" class="card-title">${results.destination}</span></p>
+                  <p><strong>Start Date: </strong><span class="date">${date}</span></p>
+                  <p><strong>Duration: </strong><span class="duration">${results.duration}</span> days</p>
+                  <p><strong>Comments: </strong><span contenteditable="true" class="comments" id="comment${results.id}">${results.comment}</span></p>
                 </div>
               </div>
             </div>
@@ -43,19 +49,43 @@ function displayData(data) {
   });
 }
 
-function deleteID(clicked) {
-  console.log('ID is: ' + clicked);
-  constructURL(clicked);
+function modifyDestination(edited) {
+  var editID = document.getElementById('destination' + edited);
+  var saveBtn = document.getElementById('save' + edited);
+
+  var originalContent = editID.innerHTML;
+  editID.addEventListener('keypress', function () {
+    if (editID.innerHTML !== originalContent) {
+      saveBtn.addEventListener('click', function () {
+        var updatedContent = editID.innerHTML;
+      });
+    }
+  });
 }
 
-function constructURL(clicked) {
-  var deleteURL = url + clicked;
-  console.log(deleteURL);
-}
+// function modifyComment(commentEdit) {
+//   var editedComment = document.getElementById('comment' + commentEdit);
+//   var saveBtn = document.getElementById('save' + commentEdit);
 
-// function deleteData() {
-//   fetch(url, {
-//     method: ‘DELETE’
-//   })
-//   .catch(() => alert("error with deletion"))
+//   var original = editedComment.innerHTML;
+//   editedComment.addEventListener('keypress', function () {
+//     if (editedComment.innerHTML !== original) {
+//       saveBtn.addEventListener('click', function () {
+//         var updated = editedComment.innerHTML;
+//       });
+//     }
+//   });
 // }
+
+//delete data from api
+function deleteData(clicked) {
+  var deleteURL = url + '/' + clicked;
+
+  fetch(deleteURL, {
+    method: 'DELETE',
+  }).catch(() => alert('error with deletion'));
+
+  setInterval(function () {
+    window.location.reload();
+  }, 1000);
+}
